e57ae4bb6dbfa89505cd5485a3e28a22f8ef3403
maulik <maulik.nitsan@gmail.com>
2022-11-18 18:35:40 +0530

    [TODO]Innital Commit schedular feature

--- ext_tables.sql
+++ ext_tables.sql
@@ -8,7 +8,7 @@ CREATE TABLE tx_cmscensus_domain_model_url
     is_auto_update_planned smallint(1) unsigned NOT NULL DEFAULT '0',
     categories             int(11) unsigned NOT NULL DEFAULT '0',
     whatcmstype            varchar(255) NOT NULL DEFAULT '',
-    httpstatus             int(11) unsigned NOT NULL DEFAULT '0',
+    httpstatus             int(11) unsigned NOT NULL DEFAULT '200',
     checkflag              int(11) unsigned NOT NULL DEFAULT '0',
 );
 
